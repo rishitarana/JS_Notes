37d23a90a2e2282dfd6a0ad04c81e7c719351979
rishitarana <102794708+rishitarana@users.noreply.github.com>
2024-11-28 06:15:32 +0000

    conversion of data types

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "true"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let itBeNumber = Number(score)
+console.log(typeof itBeNumber)
+console.log(itBeNumber)
+
+/*
+
+"33" => 33
+"33abc" => NaN
+*/
+
+ 
