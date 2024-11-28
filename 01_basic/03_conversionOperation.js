472db818115159aad1ac84922589079e33535389
rishitarana <102794708+rishitarana@users.noreply.github.com>
2024-11-28 09:36:33 +0000

    conversion Operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -3,7 +3,7 @@ let score = "true"
 console.log(typeof score)
 console.log(typeof (score))
 
-let itBeNumber = Number(score)
+let itBeNumber = String(score)
 console.log(typeof itBeNumber)
 console.log(itBeNumber)
 
@@ -13,4 +13,22 @@ console.log(itBeNumber)
 "33abc" => NaN
 */
 
- 
+//  let score2= Number(score)
+//  console.log(typeof score2)
+
+console.log("1"+2)
+console.log("1"+"2"+1)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(+true)
+console.log(+"")
+
+
+let x=4
+let y=x++
+console.log( x , y)
+
+let a=5
+let b=++a
+console.log(a , b)
