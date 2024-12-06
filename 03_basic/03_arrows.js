8e3ac02bfd36a6dd1974e71e7cf6e1da6f90f3be
rishitarana <102794708+rishitarana@users.noreply.github.com>
2024-12-06 17:30:28 +0000

    arrows commit

---
+++ 03_basic/03_arrows.js
@@ -0,0 +1,44 @@
+//Arrow Function - es6 2015  new features
+
+//this - current context
+
+const user = {
+    username :"rishita",
+    price : 999,
+
+    welcomeMess: function(){
+        console.log(`${this.username}, welcome`);
+        console.log(this)
+    }
+
+}
+// user.welcomeMess()
+// user.username = "sam" //context changed
+// user.welcomeMess()
+
+// console.log(this)
+//node environment this refers to empty object
+
+//browser global object - window
+
+
+// function chai(){
+//     let username= "ritu"
+//     console.log(this)
+// }
+// chai()
+
+
+// function chai(){
+//     let username= "ritu"
+//     console.log(this.username)
+// }
+// chai()
+
+
+
+const chai = () => {
+    let username= "ritu"
+    console.log(this)
+}
+chai()
